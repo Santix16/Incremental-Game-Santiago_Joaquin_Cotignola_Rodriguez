3223e67286643449414b613c0b4f4fffb02df962
Santix16 <santiagus0803@gmail.com>
2025-12-05 18:57:31 +0100

    The routes for the views has been created

--- Incremental-game/src/app/app.routes.ts
+++ Incremental-game/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Menu } from './views/menu/menu';
+import { Game } from './views/game/game';
+import { Settings } from './views/settings/settings';
+import { Credits } from './views/credits/credits';
+import { Stats } from './views/stats/stats';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'menu', component: Menu },
+  { path: 'game', component: Game },
+  { path: 'settings', component: Settings },
+  { path: 'credits', component: Credits },
+  { path: 'stats', component: Stats },
+];
