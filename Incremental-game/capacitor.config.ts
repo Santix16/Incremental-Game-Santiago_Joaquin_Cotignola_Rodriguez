3cb9aa2129241baf62ba13b4a8f002f5f31454ad
Santix16 <santiagus0803@gmail.com>
2026-01-19 16:44:41 +0100

    First step to create a movil version of the game

---
+++ Incremental-game/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'es.iescamp.dam.incrementalgame',
+  appName: 'Incremental-game',
+  webDir: 'dist/Incremental-game/browser'
+};
+
+export default config;
